e0fd9c8722f356d84fd750a8d44a914597094485
hendrikniemann <hendrik.niemann@kiron.ngo>
2017-12-18 18:59:06 +0100

    Add hashes to errors to generate unique id

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ Transform JSON output of a flow typecheck into JUnit XML format!
 
 At [Kiron Open Higher Education](https://kiron.ngo) our test results are reported in the JUnit test format to be displayed by our CI environment. While Jest and ESLint can be configured easily to output the JUnit XML format, flow is not able to provide this format. Instead flow can report in a custom JSON format. This utility package takes the flow output from `stdin` and returns a JUnit complaint result in the `stdout`.
 
+## Installation
+
+```
+yarn add @kironeducation/flow-junit-transformer
+```
+
 ## Usage
 
 To let flow report in JUnit format simply pipe the result of the `flow check` command through the utility library:
@@ -17,4 +23,3 @@ With `flow-bin` and `flow-junit-transformer` in your `devDependencies` it is mor
 ```
 yarn --silent flow check --json | yarn --silent flow-junit-transformer > /results/flow-raw.json
 ```
-
--- index.js
+++ index.js
@@ -1,23 +1,32 @@
+const crypto = require('crypto');
+
 module.exports = function flowJUnitTransformer(input) {
-  const errors = input.errors.map(
-    error => `
+  const errors = input.errors.map(error => {
+    const context = error.message[0].context;
+    const message = error.message.map(message => message.descr).join(' ');
+    const hash = crypto
+      .createHash('sha256')
+      .update(context + message)
+      .digest('hex')
+      .substr(0, 16);
+    return `
     <testcase
       time="0"
-      classname="org.flow.${error.level.toLowerCase()}"
+      classname="org.flow.${error.level.toLowerCase()}.${hash}"
       id="flow-error"
-      name="org.flow.${error.level.toLowerCase()}"
+      name="org.flow.${error.level.toLowerCase()}.${hash}"
       >
         <failure
           type="${error.level.toUpperCase()}"
-          message="${error.message.map(message => message.descr).join(' ')}"
+          message="${message}"
           >
             <![CDATA[
-Context: ${error.message[0].context}
-Message: ${error.message.map(message => message.descr).join(' ')}
+Context: ${context}
+Message: ${message}
             ]]>
           </failure>
-        </testcase>`,
-  );
+        </testcase>`;
+  });
 
   return `<?xml version="1.0" encoding="UTF-8" ?>
 <testsuites id="flow">

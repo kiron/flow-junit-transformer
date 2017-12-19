34847b2bd3a2dc1d6dee226f2958f921b4fef133
hendrikniemann <hendrik.niemann@kiron.ngo>
2017-12-19 19:01:13 +0100

    Return better error information
    The error info text is now much better. It uses the context and the
    location within the context to show the exact position of the error.

--- index.js
+++ index.js
@@ -1,12 +1,99 @@
+/* @flow */
 const crypto = require('crypto');
 
-module.exports = function flowJUnitTransformer(input) {
+/*::
+type FlowReport = {
+  flowVersion: string,
+  errors: ErrorReport[],
+};
+
+type ErrorReport = {
+  kind: 'infer' | 'parse' | string,
+  level: 'error' | 'warning',
+  suppressions: any[],
+  message: Message[],
+  extra?: ExtraReport[],
+};
+
+type ExtraReport = {
+  message: Message[],
+  children?: ExtraReport[],
+}
+
+type Message = BlameMessage | CommentMessage;
+
+type BlameMessage = {
+  type: 'Blame',
+  descr: string,
+  context: string,
+  line: number,
+  endline: number,
+  path: string,
+  start: number,
+  end: number,
+};
+
+type CommentMessage = {
+  type: 'Comment',
+  descr: string,
+  context: null,
+  line: number,
+  endline: number,
+  path: string,
+  start: number,
+  end: number,
+}
+*/
+
+function errorToString(error /*: ErrorReport */) /*: string */ {
+  const title = error.message.map(message => message.descr).join(' ');
+  let message = `Error: ${title}
+
+${error.message.map(messageToString).join('\n\n')}`;
+  if (error.extra) {
+    message += '\n\nExtra information - ';
+    message += error.extra.map(extraToString).join('\n');
+  }
+  return message;
+}
+
+function extraToString(extra /*: ExtraReport */) /*: string */ {
+  let message = extra.message.map(messageToString).join('\n\n');
+  if (extra.children) {
+    return message + '\n\n' + extra.children.map(extraToString).join('\n\n');
+  }
+  return message;
+}
+
+function messageToString(message /*: Message */) /*: string */ {
+  if (message.context !== null) {
+    return blameToString(message);
+  }
+  return commentToString(message);
+}
+
+function blameToString(blame /*: BlameMessage */) /*: string */ {
+  let location = '';
+  if ((blame.context, blame.end - blame.start + 1 > 0)) {
+    location = '^'.repeat(blame.end - blame.start + 1);
+  }
+  return (
+    blame.context + '\n' + ' '.repeat(Math.max(0, blame.start - 1)) + location + ' ' + blame.descr
+  );
+}
+
+function commentToString(comment /*: CommentMessage */) /*: string */ {
+  return comment.descr;
+}
+
+module.exports = function flowJUnitTransformer(input /*: FlowReport */) {
   const errors = input.errors.map(error => {
     const context = error.message[0].context;
-    const message = error.message.map(message => message.descr).join(' ');
+    const title = error.message.map(message => message.descr).join(' ');
+    const message = errorToString(error);
     const hash = crypto
       .createHash('sha256')
-      .update(context + message)
+      .update(message)
       .digest('hex')
       .substr(0, 16);
     return `
@@ -18,11 +105,10 @@ module.exports = function flowJUnitTransformer(input) {
       >
         <failure
           type="${error.level.toUpperCase()}"
-          message="${message}"
+          message="${title}"
           >
             <![CDATA[
-Context: ${context}
-Message: ${message}
+${message}
             ]]>
           </failure>
         </testcase>`;
